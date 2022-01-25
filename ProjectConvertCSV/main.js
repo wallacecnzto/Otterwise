e568057fe1a9712cc86f49d7c388223cfb739b40
Mariano <wallacewvinfo@gmail.com>
2022-01-25 15:38:20 -0300

    Fix errors and create json file

--- ProjectConvertCSV/main.js
+++ ProjectConvertCSV/main.js
@@ -4,26 +4,34 @@ let csv = fs.readFileSync("./lista.csv", "utf8")
 
 const CSVObj = (csv) => {
 
-	let lines=csv.split(/\r?\n|\r/g);
-	let result = [];
+	let lines=csv.split(/\r?\n|\r/g)
+	let result = []
   
-	let headers=lines[0].split(",");
+	let headers=lines[0].split(",")
 	headers.pop()
 	for(let i=1;i<lines.length;i++){
   
-		let obj = {};
-		let currentline=lines[i].split(",");
+		let obj = {}
+		let currentline=lines[i].split(",")
   
 		for(let j=0;j<headers.length;j++){
-			obj[headers[j]] = currentline[j];
+			obj[headers[j]] = currentline[j]
 		}
   
-		result.push(obj);
+		result.push(obj)
 		
 	}
-	return result; //JavaScript object
-	//const json = JSON.stringify(result); //JSON
+	//return result; //JavaScript object
 
-	//return	json
+	const json = JSON.stringify(result) //JSON
+
+	return	json
 }
-console.log(CSVObj(csv))
+
+//console.log(CSVObj(csv))
+
+let fileJSON = CSVObj(csv)
+
+fileJSON.toString()
+
+fs.writeFileSync('arquivo.json', fileJSON)
